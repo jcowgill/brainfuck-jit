c6b6836c666e778c5ae3e6a62628c75bdd9d5e66
James Cowgill <james410@cowgill.org.uk>
2013-01-08 00:31:15 +0000

    Use spaces instead of tabs

--- BfCompiler.h
+++ BfCompiler.h
@@ -9,27 +9,27 @@
 
 namespace bf
 {
-	// Information about the type of code which represents an EOF
-	struct EofCode
-	{
-		// True to return an eof code (false means that nothing is modified)
-		bool modifyValue;
+    // Information about the type of code which represents an EOF
+    struct EofCode
+    {
+        // True to return an eof code (false means that nothing is modified)
+        bool modifyValue;
 
-		// The code (only used if modifyValue is true)
-		std::int32_t code;
-	};
+        // The code (only used if modifyValue is true)
+        std::int32_t code;
+    };
 
-	// Compiles a brainfuck program to machine code
-	//  input      = the input stream to read the program from
-	//  outputCode = memory region to write code to
-	//  heap       = address of heap to be used by the program
-	//  cellSize   = size of each cell
-	//  eofCode    = code to produce for eof situations
-	void Compile(std::istream const& input, void * outputCode, void * heap, int cellSize, EofCode eofCode);
+    // Compiles a brainfuck program to machine code
+    //  input      = the input stream to read the program from
+    //  outputCode = memory region to write code to
+    //  heap       = address of heap to be used by the program
+    //  cellSize   = size of each cell
+    //  eofCode    = code to produce for eof situations
+    void compile(std::istream const& input, void * outputCode, void * heap, int cellSize, EofCode eofCode);
 
-	// See above
-	//  Defaults to cell size of 1, and an eof code of 0
-	void Compile(std::istream const& input, void * outputCode, void * heap, int cellSize = 1);
+    // See above
+    //  Defaults to cell size of 1, and an eof code of 0
+    void compile(std::istream const& input, void * outputCode, void * heap, int cellSize = 1);
 }
 
 #endif
